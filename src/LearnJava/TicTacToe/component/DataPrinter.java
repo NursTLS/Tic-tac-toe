d69ffda2cefeb124e7b66e31115ad8b15ab21a7f
NursTLS <107918147+NursTLS@users.noreply.github.com>
2022-07-10 17:35:07 +0600

    Implement DataPrinter component

--- src/LearnJava/TicTacToe/component/DataPrinter.java
+++ src/LearnJava/TicTacToe/component/DataPrinter.java
@@ -19,6 +19,7 @@
 
 package LearnJava.TicTacToe.component;
 
+import LearnJava.TicTacToe.model.Cell;
 import LearnJava.TicTacToe.model.GameTable;
 
 /**
@@ -27,10 +28,23 @@ import LearnJava.TicTacToe.model.GameTable;
  */
 public class DataPrinter {
     public void printMappingTable() {
-
+        System.out.println("-------------");
+        System.out.println("| 7 | 8 | 9 |");
+        System.out.println("-------------");
+        System.out.println("| 4 | 5 | 6 |");
+        System.out.println("-------------");
+        System.out.println("| 1 | 2 | 3 |");
+        System.out.println("-------------");
     }
 
     public void printGameTable(final GameTable gameTable) {
-
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
